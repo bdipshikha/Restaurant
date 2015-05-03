ebe919515a40b5bdb8f9c759ec74b0badf34f2af
Dipshikha Biswas <Dipshikha@Dipshikhas-MacBook-Pro.local>
2015-05-03 10:42:27 -0400

    Push1

---
+++ public/scripts/scripts.js
@@ -0,0 +1,284 @@
+var addNewDishButton = document.getElementById("addNewDish");
+addNewDishButton.addEventListener("click", function() {
+	var newName = document.getElementById("newDishName");
+	var newImage_url = document.getElementById("newDishImage_url");
+	var newPrice = document.getElementById("newDishPrice");
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", "http://localhost:3000/dishes");
+	xhr.setRequestHeader("Content-Type", "application/json");
+	xhr.addEventListener("load", function() {
+		var returnedDish = JSON.parse(xhr.response);
+		showDish(returnedDish);
+		newName.value = "";
+		newImage_url = "";
+		newPrice = "";
+	});
+
+	var newDish = {
+		name: newName.value,
+		image_url: newImage_url.value,
+		price: newPrice.value
+	}
+	xhr.send(JSON.stringify(newDish));
+})
+
+var createLiForDish = function(li, dish) {
+	li.innerHTML = "";
+	li.setAttribute("id", "dish" + dish.id );
+	var dishText = dish.name + " " + dish.price;
+	var dishTextNode = document.createTextNode(dishText);
+
+    var spanForText = document.createElement("span");
+    spanForText.appendChild(dishTextNode);
+    spanForText.setAttribute("style", "margin:10px;");
+    li.appendChild(spanForText);
+
+    var dishImg = document.createElement("img");
+    dishImg.setAttribute('src', dish.image_url);
+    var spanForImg = document.createElement("span");
+    spanForImg.appendChild(dishImg);
+    spanForImg.setAttribute('style', "width:200px; height:100px;");
+    li.appendChild(spanForImg);
+ 
+
+ var editButton = document.createElement("button");
+ editButton.innerText = "Edit";
+ editButton.addEventListener("click", function(){
+ 	editDish(li, dish.name, dish.image_url, dish.price);
+ });
+ li.appendChild(editButton);
+
+ var deleteButton = document.createElement("button");
+ deleteButton.innerText = "Delete";
+ deleteButton.addEventListener("click", deleteDish);
+ 
+ li.appendChild(deleteButton);
+}
+
+var showDish = function(dish) {
+	var li = document.createElement("li");
+	createLiForDish(li, dish);
+	var ul = document.getElementById("dishesList");
+	ul.appendChild(li);
+};
+
+var showAllDishes = function() {
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", "http://localhost:3000/dishes");
+    xhr.addEventListener("load", function() {
+        var dishes = JSON.parse(xhr.response);
+        dishes.forEach(function(dish) {
+            showDish(dish);
+        });
+    });
+    xhr.send();
+}
+var editDish = function(li, name, image_url, price) {
+    li.innerHTML = "";
+    var id = li.id.substring(4); 
+
+    var nameField = document.createElement("input");
+    nameField.setAttribute("type", "text");
+    nameField.value = name;
+    li.appendChild(nameField);
+
+
+    
+
+    var image_urlField = document.createElement("input");
+    image_urlField.setAttribute("type", "text");
+    image_urlField.value = image_url;
+    li.appendChild(image_urlField);
+
+    
+
+    var priceField = document.createElement("input");
+    priceField.setAttribute("type", "text");
+    priceField.value = price;
+    li.appendChild(priceField);
+
+
+    var updateButton = document.createElement("button");
+    updateButton.innerText = "Update";
+    updateButton.addEventListener("click", function() {
+        var newName = nameField.value;
+        var newImage_url = image_urlField.value;
+        var newPrice = priceField.value;
+        updateDish(li, newName, newImage_url, newPrice);
+    });
+    li.appendChild(updateButton);
+};
+
+var updateDish = function(li, newName, newImage_url, newPrice) {
+
+    var id = li.id.substring(4);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("PUT", "http://localhost:3000/dish/" + id);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.addEventListener("load", function() {
+        var returnedDish = JSON.parse(xhr.response);
+        alert(xhr.response)
+        createLiForDish(li, returnedDish);
+    });
+
+    var updatedDish = {
+            name: newName,
+            image_url: newImage_url,
+            price: newPrice
+        } 
+    //alert(JSON.stringify(updatedDish));
+    xhr.send(JSON.stringify(updatedDish));
+};
+
+var deleteDish = function() {
+    var li = this.parentNode; 
+    var id = li.id.substring(4);
+  
+    var xhr = new XMLHttpRequest();
+    xhr.open("DELETE", "http://localhost:3000/dish/" + id);
+    xhr.addEventListener("load", function() {
+        if (JSON.parse(xhr.responseText).deleted === true) {
+            li.remove();
+        }
+    });
+    
+    xhr.send();
+}
+
+////////////////////////////////////////
+// category starts here
+
+
+var addNewCategoryButton = document.getElementById("addNewCategory");
+addNewCategoryButton.addEventListener("click", function() {
+    var newName = document.getElementById("newCategoryName");
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost:3000/categories");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.addEventListener("load", function() {
+        var returnedCategory = JSON.parse(xhr.response);
+        showCategory(returnedCategory);
+        newName.value = "";
+        
+    });
+
+    var newCategory = {
+        name: newName.value,
+        
+    }
+    xhr.send(JSON.stringify(newCategory));
+})
+
+var createLiForCategory = function(li, category) {
+    li.innerHTML = "";
+    li.setAttribute("id", "category" + category.id );
+    var categoryText = category.name;
+    var categoryTextNode = document.createTextNode(categoryText);
+
+    var link = document.createElement("a");
+    link.setAttribute("href", "category/" + category.id);
+    link.innerText = category.name;
+
+    var spanForText = document.createElement("span");
+    spanForText.appendChild(categoryTextNode);
+    spanForText.setAttribute("style", "margin:10px;");
+    li.appendChild(spanForText);
+ 
+
+ var editButton = document.createElement("button");
+ editButton.innerText = "Edit";
+ editButton.addEventListener("click", function(){
+    editCategory(li, category.name);
+ });
+ li.appendChild(editButton);
+
+ var deleteButton = document.createElement("button");
+ deleteButton.innerText = "Delete";
+ deleteButton.addEventListener("click", deleteCategory);
+ 
+ li.appendChild(deleteButton);
+}
+
+var showAllCategories = function() {
+    console.log("Works - line 206")
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://localhost:3000/categories");
+    xhr.addEventListener("load", function() {
+        var categories = JSON.parse(xhr.response);
+        categories.forEach(function(category) {
+            showCategory(category)
+        });
+    });
+    xhr.send();
+}
+
+var showCategory = function(category) {
+    var li = document.createElement("li");
+    createLiForCategory(li, category);
+    var ul = document.getElementById("categoriesList");
+    ul.appendChild(li);
+};
+
+var editCategory = function(li, name) {
+    li.innerHTML = "";
+    var id = li.id.substring(8); 
+
+    var nameField = document.createElement("input");
+    nameField.setAttribute("type", "text");
+    nameField.value = name;
+    li.appendChild(nameField);
+
+
+    var updateButton = document.createElement("button");
+    updateButton.innerText = "Update";
+    updateButton.addEventListener("click", function() {
+        var newName = nameField.value;
+        
+        updateCategory(li, newName);
+    });
+    li.appendChild(updateButton);
+};
+
+var updateCategory = function(li, newName) {
+
+    var id = li.id.substring(8);
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("PUT", "http://localhost:3000/category/" + id);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.addEventListener("load", function() {
+        var returnedCategory = JSON.parse(xhr.response);
+        alert(xhr.response)
+        createLiForCategory(li, returnedCategory);
+    });
+
+    var updatedCategory = {
+            name: newName,
+        } 
+  
+    xhr.send(JSON.stringify(updatedCategory));
+};
+
+var deleteCategory = function() {
+    var li = this.parentNode; 
+    var id = li.id.substring(8);
+  
+    var xhr = new XMLHttpRequest();
+    xhr.open("DELETE", "http://localhost:3000/category/" + id);
+    xhr.addEventListener("load", function() {
+        if (JSON.parse(xhr.responseText).deleted === true) {
+            li.remove();
+        }
+    });
+    
+    xhr.send();
+}
+
+showAllDishes();
+
+showAllCategories();
+
+
---
+++ server.js
@@ -0,0 +1,147 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var sqlite3 = require("sqlite3").verbose();
+var cors = require('cors');
+
+var db = new sqlite3.Database("db/diner.db");
+var app = express();
+
+app.use(cors());
+app.use(bodyParser.json({ extended: false }));
+
+app.use(express.static('public'));
+app.get('/', function(req, res){
+	res.render('home.html')
+});
+
+app.get('/categories', function(req, res){
+	db.all("SELECT * FROM categories", function(err, rows){
+		console.log("it works-line 19")
+		if(err){
+			throw err;
+		}
+		res.json(rows);
+	});
+});
+
+app.get('/category/:id', function(req, res){
+	var id = req.params.id;
+	db.get('SELECT * FROM categories WHERE id = ?', req.params.id, function(err, row){
+	var category_id = req.params.id;	
+		db.all("SELECT dishes.id, dishes.name FROM dishes INNER JOIN categories ON "+
+			+"categories.id = dishes.category_id;"), function(err, dishes) {
+
+		if(err){
+			throw err;
+		}
+		res.json(row);
+		};
+	});
+});
+
+app.post('/categories', function(req, res){
+	db.run("INSERT INTO categories (name) VALUES (?)", req.body.name, function(err,row){
+		if(err){
+			throw err;
+		}
+		var id = this.lastID;
+        db.get("SELECT * FROM categories WHERE id = ?", id, function(err, row) {
+        	if(err) {
+        		throw err;
+        	}
+        	res.json(row);
+        });
+    });
+});
+
+app.put('/category/:id', function(req, res){
+	var id = req.params.id;
+	console.log("it works-line 59")
+	db.run("UPDATE categories SET name = ? WHERE id = ?", req.body.name, id, function(err){
+		console.log("it works-line 61")
+		if(err){
+			throw err;
+		}
+		db.get("SELECT * FROM categories WHERE id = ?", id, function(err, row){
+			console.log("it works-line 66")
+			if(err){
+				throw err;
+			}
+			res.json(row);
+		});
+	});
+});
+
+app.delete('/category/:id', function(req, res){
+	db.run("DELETE FROM categories WHERE id = ?", req.params.id, function(err){
+		if(err){
+			throw err;
+		}
+		res.json({deleted: true});
+	});
+});
+
+
+app.get('/dishes', function(req, res) {
+	db.all("SELECT * FROM dishes", function(err, rows) {
+		if(err) {
+			throw err;
+		}
+		res.json(rows);
+	});
+});
+
+
+app.get('/dish/:id', function(req, res) {
+	db.get("SELECT * FROM dishes WHERE id = ?", req.params.id, function(err, row){
+		if(err) {
+			throw err;
+		}
+		res.json(row);
+	});
+});
+
+app.post('/dishes', function(req, res) {
+	db.run("INSERT INTO dishes (name, price, image_url, category_id) VALUES (?,?,?,?)", req.body.name, req.body.price, req.body.image_url, req.body.category_id, function(err) {
+		if(err) {
+			throw err;
+		}
+    var id = this.lastID;
+    db.get("SELECT * FROM dishes WHERE id = ?", id, function(err, row) {
+    	if(err) {
+    		throw err;
+    	}
+    	res.json(row);
+    });
+  });
+});
+
+app.put('/dish/:id', function(req, res) {
+	var id = req.params.id;
+	db.run("UPDATE dishes SET name = ?, image_url = ?, price = ?, category_id = ? WHERE id = ?", req.body.name, req.body.image_url, req.body.price, req.body.category_id, id, function (err) {
+		if(err) {
+			throw err;
+		}
+		db.get("SELECT * FROM dishes WHERE id = ?", id, function(err, row) {
+			if(err) {
+				throw err;
+			}
+			res.json(row);
+		});
+	});
+});
+
+app.delete('/dish/:id', function(req, res) {
+	db.run("DELETE FROM dishes WHERE id = ?", req.params.id, function(err) {
+		if(err) {
+			throw err;
+		}
+		res.json({deleted: true});
+	});
+});
+
+app.listen(3000);
+console.log('Listening on port 3000');
+
+
+
